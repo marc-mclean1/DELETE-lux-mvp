440a6af846cceca1366a30298da00a0297147b92
Anurag Parashar <anurag.parashar@wavemaker.com>
2017-06-01 21:57:14 +0000

    Changes to integrate pdfs and security roles, landing pages Reviewer: Marc New files added - [Test] Files modified - [header,HelpWithChat,securityService,profiles,project-security.xml,app.variables.json,pages-config.json]

--- src/main/webapp/pages/HelpWithChat/HelpWithChat.js
+++ src/main/webapp/pages/HelpWithChat/HelpWithChat.js
@@ -1,8 +1,8 @@
-Application.$controller("HelpWithChatPageController", ["$scope", function ($scope) {
+Application.$controller("HelpWithChatPageController", ["$scope", function($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function () {
+    $scope.onPageReady = function() {
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
@@ -12,6 +12,24 @@ Application.$controller("HelpWithChatPageController", ["$scope", function ($scop
          * e.g. to get value of text widget named 'username' use following script
          * '$scope.Widgets.username.datavalue'
          */
+    };
+
+    $scope.watsonChatbot2Watsonresponse = function($isolateScope) {
+        var data = $isolateScope.watsonresponse;
+        var output_text = _.get(data, 'output.text');
+
+        /* check for a message from chat */
+        if (Array.isArray(output_text)) {
+            _.forEach(output_text, function(text) {
+
+                if (_.includes(text, ".pdf")) {
+                    $scope.sourceURL = "resources/files/" + text.substring(0, text.indexOf(".pdf") + 4);
+                }
+            });
+        } else {
+            //handle the string
+        }
+
     };
 
 }]);
