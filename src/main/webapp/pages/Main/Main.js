67fe15ab4aad62e9f980158f971fa8edbc5acb24
Anurag Parashar <anurag.parashar@wavemaker.com>
2017-06-07 18:50:38 +0000

    Deployed application is not working, deploying a new copy. Files modified - [Main,HelpWithChat,watsonChatbot]

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -1,7 +1,6 @@
 Application.$controller("MainPageController", ["$scope", function($scope) {
     "use strict";
 
-    var imageBaseURL = "http://img.michaels.com/";
     /* perform any action on widgets/variables within this block */
     $scope.onPageReady = function() {
         /*
@@ -15,201 +14,42 @@ Application.$controller("MainPageController", ["$scope", function($scope) {
          */
     };
 
-    //sample request body for nlu service
-    $scope.requestBodyNLU = {
-        "text": "",
-        "features": {
-            "entities": {
-                "emotion": true,
-                "sentiment": true,
-                "limit": 10
-            },
-            "keywords": {
-                "emotion": true,
-                "sentiment": true,
-                "limit": 10
-            },
-            "categories": {
-                "emotion": true,
-                "sentiment": true,
-                "limit": 10
-            }
-        }
-    };
-
-    $scope.requestBodyConversation = {
-        "input": {
-            "text": ""
-        },
-        "alternate_intents": true
-    };
-
-    //on message form submit set the requestbody for the nlu service and invoke it.
-    $scope.messageFormSubmit = function($event, $isolateScope, $formData) {
-        var message = _.get($scope.Widgets, 'messageForm.formWidgets.message.datavalue') || _.get($formData, 'message');
-        if (!message) {
-            return;
-        }
-        $scope.requestBodyNLU.text = message;
-        $scope.requestBodyConversation.input.text = message;
-        $scope.Variables.invokeNLUService.setInput('RequestBody', $scope.requestBodyNLU);
-        $scope.Variables.invokeConversationService.setInput('RequestBody', $scope.requestBodyConversation);
-        $scope.Variables.invokeConversationService.invoke();
-        $scope.Variables.invokeNLUService.invoke();
-    };
-
-    //livelist calls this function to get the image url writtern in the use expression of the picture widget in listtemplate
-    $scope.getProductImageUrl = function(product) {
-        var originalImg = _.get(product, 'images.image-group.0.image'),
-            smallImg,
-            picurl,
-            newImg = new Image,
-            pictureWidget = this.currentItemWidgets.Picture.$element.find('img'),
-            loadingIconSrc = 'resources/images/imagelists/loading.gif';
-
-        //if the image-group doesn't exist then just return empty string
-        if (!originalImg) {
-            return '';
-        }
-
-        //if it is an array then get the first object
-        if (_.isArray(originalImg)) {
-            originalImg = originalImg[0];
-        }
-
-        smallImg = (_.get(originalImg, '@path') || '').split("|")[0] + "|240:240";
-
-        picurl = imageBaseURL + '/' + smallImg;
-
-        //on load of the image just change the source of the pictureWidget
-        newImg.onload = function() {
-            pictureWidget[0].src = picurl;
-        };
-
-        //this will load the image asynchronously
-        newImg.src = picurl;
-
-        //when navigated this will make the old image again to loading state
-        pictureWidget[0].src = loadingIconSrc;
-
-        return loadingIconSrc;
-    };
-
-    $scope.getProductName = function(product) {
-        return product['display-name'] || '';
-    };
-
-    $scope.getProductScore = function(product) {
-        return product.score;
-    };
-
-    $scope.getProductRating = function(product) {
-        return _.get(product, 'bvAverageRating') || '';
-    };
-
-
-    $scope.discoverMoreClick = function($event, $isolateScope) {
-        //on discover click
-        var keywordParams = [],
-            entityParams = [];
-
-        var convEntities = ['color', 'brand'];
-
-        _.forEach($scope.Widgets.entityCheckbox.datavalue, function(entity) {
-            pushToArrayIfNotPresent(entityParams, 'enriched_long-description.entities.type', entity.type);
-            pushToArrayIfNotPresent(entityParams, 'enriched_long-description.entities.text', entity.text);
-        });
-
-
-        _.forEach($scope.Widgets.entity_ConversationCheckbox.datavalue, function(entity) {
-            if (_.includes(convEntities, entity.entity)) {
-                pushToArrayIfNotPresent(entityParams, entity.entity, entity.value);
-            } else {
-                pushToArrayIfNotPresent(entityParams, 'enriched_long-description.entities.text', entity.value);
-            }
-        });
-
-        //build comma separated keyword params for keywords
-        _.forEach($scope.Widgets.keywordCheckbox.datavalue, function(keyword) {
-            pushToArrayIfNotPresent(keywordParams, 'enriched_long-description.keywords.text', keyword.text);
-        });
-        console.log(entityParams, keywordParams);
-        $scope.Variables.invokeDiscoveryServiceWFilter.setInput('query', keywordParams.length ? (keywordParams.join(',')) : "");
-        $scope.Variables.invokeDiscoveryServiceWFilter.setInput('filter', entityParams.length ? (entityParams.join(',')) : "");
-        $scope.Variables.invokeDiscoveryServiceWFilter.invoke();
-    };
-
-    function pushToArrayIfNotPresent(array, key, value) {
-        if (value) {
-            var str = key + ':"' + value + '"';
-            if (!_.includes(array, str))
-                array.push(str);
-        }
-    }
-
-    $scope.invokeNLUServiceonSuccess = function(variable, data) {
-        console.log(variable, data);
-        var keywordParams = [],
-            entityParams = [];
-
-        _.forEach(data.entities, function(entity) {
-            pushToArrayIfNotPresent(entityParams, 'enriched_long-description.entities.type', entity.type);
-            pushToArrayIfNotPresent(entityParams, 'enriched_long-description.entities.text', entity.text);
-        });
-        _.forEach(data.keywords, function(keyword) {
-            pushToArrayIfNotPresent(keywordParams, 'enriched_long-description.keywords.text', keyword.text);
-        });
-        console.log(entityParams, keywordParams);
-        $scope.Variables.invokeDiscoveryServiceWFilter.setInput('query', keywordParams.length ? (keywordParams.join(',')) : "");
-        $scope.Variables.invokeDiscoveryServiceWFilter.setInput('filter', entityParams.length ? (entityParams.join(',')) : "");
-        $scope.Variables.invokeDiscoveryServiceWFilter.invoke();
-
-    };
-
-
-    $scope.discoveryListSetrecord = function($event, $isolateScope, $index, $data) {
-        $("html, body").animate({
-            scrollTop: 0
-        }, "fast");
-    };
-
-
-    $scope.watsonChatbot1Watsonresponse = function($isolateScope) {
-        var convEntities = ['color', 'brand'];
-        var entityParams = [];
+    $scope.watsonChatbot2Watsonresponse = function($isolateScope) {
         var data = $isolateScope.watsonresponse;
-        var conversationSuccess = false;
-        if (data && data.entities && data.entities.length) {
-            _.forEach(data.entities, function(entity) {
-                if (entity.confidence > 0.6) {
-                    if (_.includes(convEntities, entity.entity)) {
-                        pushToArrayIfNotPresent(entityParams, entity.entity, entity.value);
-                    } else {
-                        pushToArrayIfNotPresent(entityParams, 'enriched_long-description.entities.text', entity.value);
+        var output_text = _.get(data, 'output.text');
+
+        /* check for a message from chat */
+        if (Array.isArray(output_text)) {
+            _.forEach(output_text, function(text, index) {
+
+                if (_.includes(text, ".pdf]")) {
+                    // pull out the entire PDF
+                    var pos = text.indexOf(".pdf]");
+                    for (var i = pos; i >= 0; i--) {
+                        if (text[i] == "[") {
+                            var pdf = text.substring(i + 1, pos + 4);
+                            console.log("pos = " + pos);
+                            console.log("PDF = " + pdf);
+                            // only handle if an open bracket is found, otherwise, we do nothing.
+                            $scope.sourceURL = "resources/files/" + pdf.replace(/ /g, '_');
+                            data.output.text[index] = text.replace("[" + pdf + "]", pdf);
+                            break;
+                        }
                     }
-                    conversationSuccess = true;
                 }
             });
-            console.log(entityParams, conversationSuccess);
-
-            // TODO remove comments
-            /*
-            if (conversationSuccess) {
-                $scope.Variables.invokeDiscoveryServiceWFilter.setInput('filter', entityParams.length ? (entityParams.join(',')) : "");
-                $scope.Variables.invokeDiscoveryServiceWFilter.invoke();
-            }
-            */
+        } else {
+            //handle the string - this should never happen.
         }
-
-        // TODO remove comments
-        /*
-        if (data.input.text && !conversationSuccess) {
-            $scope.requestBodyNLU.text = data.input.text;
-            $scope.Variables.invokeNLUService.setInput('RequestBody', $scope.requestBodyNLU);
-            $scope.Variables.invokeNLUService.invoke();
-        }
-        */
-
     };
+    $scope.sendYes = function() {
+        alert('Yes');
+        //$scope.Widgets.watsonChatbot2.send('yes');
+    }
 
 }]);
+
+function mySend() {
+    console.log('sending');
+    $('[data-ng-controller="WatsonChatbotController"]').scope().send('No');
+}
